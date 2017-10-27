6e37ec4845e5b2af439b1509339f625bfef6db12
yaoyuchiang <32789860+yaoyuchiang@users.noreply.github.com>
2017-10-27 00:32:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//alert("Hello JS 1")
+
+//function showMsg(msg1,msg2){
+//var MsgAll=msg1+ "+" +msg2;
+//return MsgAll;
+//}
+
+
+function launch(M,N){
+    console.log(M,N);
+    var launch = Math.floor(Math.random()*(M-N+1)+N);
+    return random;
+}
